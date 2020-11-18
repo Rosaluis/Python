67fcdd28f6a87bc0e4bc4e45a4200ba93866f21d
Alois <alois.rosecky@gmail.com>
2020-11-18 21:38:43 +0100

    Rozšířená funkční verze s filtrováním. Vyfiltrovávání omezeno jen na jméno a popis dílu.

--- DemagPartsView/PartsView.py
+++ DemagPartsView/PartsView.py
@@ -43,7 +43,7 @@ from sqlite3 import Error
 from string import Template
 from PyQt5 import QtGui
 from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QRadioButton, QGroupBox, QVBoxLayout,\
-    QGridLayout, QLineEdit, QPushButton, QDialogButtonBox, QTabWidget, QLabel, QCheckBox, QTextEdit
+    QGridLayout, QLineEdit, QPushButton, QTabWidget, QLabel, QCheckBox, QTextEdit
 import sys
 from PyQt5.QtCore import Qt
 
@@ -82,28 +82,41 @@ class Window(QWidget):
         self.mainLay.addWidget(self.grpOutput)
         self.mainLay.addWidget(self.grpFoot)
         
-        
+        self.lbComplete = QLabel("%")
+        self.lbComplete.setStyleSheet(".QLabel{margin-left:50%; margin-right:50%}")
+        self.lbAdd = QLabel("+")
+        self.lbAdd.setStyleSheet(".QLabel{margin-left:50%; margin-right:50%}")
         self.lbPartName = QLabel("name: ")
         self.lePartName = QLineEdit()
         self.lePartName.setText("1B6")
         self.lbPartDescription = QLabel("description: ")
         self.lePartDescription = QLineEdit()
-        self.lbStroj = QLabel("machine: ")
-        self.leStroj = QLineEdit()
-        self.cbPartNumber = QCheckBox()
         self.cbPartName = QCheckBox()
-        self.cbStroj = QCheckBox()
+        self.cbPartName.setStyleSheet(".QCheckBox{margin-left:50%; margin-right:50%}")
+        self.cbPartDescription = QCheckBox()
+        self.cbPartDescription.setStyleSheet(".QCheckBox{margin-left:50%; margin-right:50%}")
+        self.cbCompleteName = QCheckBox()
+        self.cbCompleteName.setStyleSheet(".QCheckBox{margin-left:50%; margin-right:50%}")
+        self.cbCompleteDesc = QCheckBox()
+        self.cbCompleteDesc.setStyleSheet(".QCheckBox{margin-left:50%; margin-right:50%}")
+        self.cbCompleteDesc.setChecked(True)
+        self.chClearOutput = QCheckBox("clearing")
+        self.chClearOutput.setChecked(True)
+        self.btClearOutput = QPushButton("clear")
         self.btCommit = QPushButton("commit")
+        
+        self.grdLayParams.addWidget(self.lbComplete,            0, 4, 1, 1)
+        self.grdLayParams.addWidget(self.lbAdd,              0, 5, 1, 1)
         self.grdLayParams.addWidget(self.lbPartName,            1, 1, 1, 1)
         self.grdLayParams.addWidget(self.lePartName,            1, 2, 1, 2)
-        self.grdLayParams.addWidget(self.cbPartNumber,          1, 4, 1, 1)
+        self.grdLayParams.addWidget(self.cbCompleteName,        1, 4, 1, 1)
+        self.grdLayParams.addWidget(self.cbPartName,            1, 5, 1, 1)
         self.grdLayParams.addWidget(self.lbPartDescription,     2, 1, 1, 1)
         self.grdLayParams.addWidget(self.lePartDescription,     2, 2, 1, 2)
-        self.grdLayParams.addWidget(self.cbPartName,            2, 4, 1, 1)
-        self.grdLayParams.addWidget(self.lbStroj,               3, 1, 1, 1)
-        self.grdLayParams.addWidget(self.leStroj,               3, 2, 1, 2)
-        self.grdLayParams.addWidget(self.cbStroj,               3, 4, 1, 1)
-        self.grdLayParams.addWidget(self.btCommit,              4, 5, 1, 1)
+        self.grdLayParams.addWidget(self.cbCompleteDesc,        2, 4, 1, 1)
+        self.grdLayParams.addWidget(self.cbPartDescription,     2, 5, 1, 1)
+        self.grdLayParams.addWidget(QLabel(""),                 4, 1, 1, 1)
+        self.grdLayParams.addWidget(self.btCommit,              5, 5, 1, 1)
         
         self.tabsOutput = QTabWidget()
         self.tabOutputData = QWidget()
@@ -111,6 +124,8 @@ class Window(QWidget):
         self.teOutputData.setMinimumWidth(600)
         self.tabOutputData.setLayout(self.grdLayTabOutData)
         self.grdLayTabOutData.addWidget(self.teOutputData,      0, 0, 10, 10)
+        self.grdLayTabOutData.addWidget(self.chClearOutput,     11,8, 1,  1)
+        self.grdLayTabOutData.addWidget(self.btClearOutput,     11,9 ,1,  1)
         
         self.tabOutputCmd = QWidget()
         self.teOutputCmd = QTextEdit()
@@ -134,23 +149,50 @@ class Window(QWidget):
         self.show()
         
         self.btCommit.clicked[bool].connect(self.commitDBCommand)
+        self.btClearOutput.clicked[bool].connect(self.clearOutput)
+        
+    def clearOutput(self):
+        self.teOutputData.clear()
         
     def commitDBCommand(self):
         database = r"Parts.sqlite3"
         # create a database connection
         conn = self.create_connection(database)
-        data = self.lePartName.text()
+        dataName = self.lePartName.text()
+        dataDesc = self.lePartDescription.text()
+        
+        if self.chClearOutput.isChecked() == True:
+            self.teOutputData.clear()
     
         sql = Template('''
             SELECT parts.partName, parts.partDescription, kinds.kindName, machines.machineName, parts.partLink
             FROM parts
             INNER JOIN kinds ON parts.partKind = kinds.kindID
             INNER JOIN machines ON parts.partMachine = machines.machineId
-            WHERE parts.partName LIKE \"$what\" ''')
+            WHERE parts.partName LIKE \"$pName\" and
+            parts.partDescription LIKE \"$pDesc\"
+            ''')
         
         cur = conn.cursor()
-        cur.execute(sql.substitute(what = data)) #https://docs.python.org/2/library/string.html#template-strings
-        print(cur)
+        #https://docs.python.org/2/library/string.html#template-strings
+        if self.cbPartName.isChecked() == True and self.cbPartDescription.isChecked() == False:
+            if self.cbCompleteName.isChecked() == True:
+                cur.execute(sql.substitute(     pName = "%" + dataName + "%",        pDesc = "%")) 
+            else:
+                cur.execute(sql.substitute(     pName = dataName,                    pDesc = "%"))  
+                  
+        elif self.cbPartName.isChecked() == False and self.cbPartDescription.isChecked() == True:
+            if self.cbCompleteDesc.isChecked() == True:
+                cur.execute(sql.substitute(     pName = "%",                         pDesc = "%"+ dataDesc + "%"))
+            else:
+                cur.execute(sql.substitute(     pName = "%",                         pDesc = dataDesc))
+                
+        elif self.cbPartName.isChecked() == True and self.cbPartDescription.isChecked() == True and len(self.lePartDescription.text()) > 0:
+            cur.execute(sql.substitute(         pName = "%" + dataName + "%",        pDesc = "%"+ dataDesc + "%"))
+        
+        
+        
+#         print(cur)
         
         rows = cur.fetchall()
         for row in rows:
